ccaa92cfa70619587fedbbbefaf78503e9694d06
takahiro-tsuchiya-woven <takahiro.tsuchiya@woven.toyota>
2024-04-19 14:29:43 +0900

    Fixed convert_fahrenheit_to_celsius function

--- 03_version_control_advanced/01_challenge/python/tconv.py
+++ 03_version_control_advanced/01_challenge/python/tconv.py
@@ -9,5 +9,5 @@ def convert_fahrenheit_to_celsius():
     print("Enter the temperature in Fahrenheit:")
     fahrenheit = input()
     fahrenheit = int(fahrenheit)
-    celsius = (fahrenheit * 9/5) + 32
+    celsius = 5 / 9 * (fahrenheit - 32)
     print(f"The Fahrenheit temperature {fahrenheit} you entered is {celsius} in Fahrenheit.")
