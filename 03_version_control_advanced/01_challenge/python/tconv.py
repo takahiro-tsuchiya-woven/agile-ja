803287b398654d8cf9a56a6314565ee0eb043518
takahiro-tsuchiya-woven <takahiro.tsuchiya@woven.toyota>
2024-04-23 15:53:30 +0900

    Add time library

--- 03_version_control_advanced/01_challenge/python/tconv.py
+++ 03_version_control_advanced/01_challenge/python/tconv.py
@@ -1,8 +1,11 @@
+import time
+
 def convert_celsius_to_fahrenheit():
     print("Enter the temperature in Celsius:")
     celsius = input()
     celsius = int(celsius)
     fahrenheit = (celsius * 9/5) + 32
+    time.sleep(5)
     print(f"The Celsius temperature {celsius} you entered is {fahrenheit} in Fahrenheit.")
 
 def convert_fahrenheit_to_celsius():
@@ -10,6 +13,7 @@ def convert_fahrenheit_to_celsius():
     fahrenheit = input()
     fahrenheit = int(fahrenheit)
     celsius = 5 / 9 * (fahrenheit - 32)
+    time.sleep(5)
     print(f"The Fahrenheit temperature {fahrenheit} you entered is {celsius} in Fahrenheit.")
 
 def kelvin_to_fahrenheit():
@@ -17,6 +21,7 @@ def kelvin_to_fahrenheit():
     kelvin = input()
     kelvin = int(kelvin)
     fahrenheit = (kelvin - 273.15) * 9/5 + 32
+    time.sleep(5)
     print(f"The Kelvin temperature {kelvin} you entered is {fahrenheit} in Fahrenheit.")
 
 def fahrenheit_to_kelvin():
@@ -24,6 +29,7 @@ def fahrenheit_to_kelvin():
     fahrenheit = input()
     fahrenheit = int(fahrenheit)
     kelvin =  (fahrenheit - 32) * 5/9 + 273.15
+    time.sleep(5)
     print(f"The Fahrenheit temperature {fahrenheit} you entered is {kelvin} in Kelvin.")
 
 def kelvin_to_celsius():
@@ -31,6 +37,7 @@ def kelvin_to_celsius():
     kelvin = input()
     kelvin = int(kelvin)
     celsius = kelvin - 273.15
+    time.sleep(5)
     print(f"The Kelvin temperature {kelvin} you entered is {celsius} in Celsius.")
 
 def celsius_to_kelvin():
@@ -38,6 +45,7 @@ def celsius_to_kelvin():
     celsius = input()
     celsius = int(celsius)
     kelvin = celsius + 273.15
+    time.sleep(5)
     print(f"The Celsius temperature {celsius} you entered is {kelvin} in Kelvin.")
 
 def main():
